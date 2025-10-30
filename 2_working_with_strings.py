1617096c3b54d619bb04e7dc0e895bee2dcc5709
Flipout23 <whenlimit11@gmail.com>
2025-10-30 14:32:00 +0000

    advanced strings

--- 2_working_with_strings.py
+++ 2_working_with_strings.py
@@ -105,11 +105,13 @@ print(phrase4[-1])
 # # String Methods
 # # ----------------------------------------
 
-# sentence = "Python is fun to learn"
+sentence = "Python is fun to learn"
 
 # # .split(): Splits the string into a list of words
-# words = sentence.split()
-# print("Split result:", words)
+words = sentence.split()
+print("Split result:", words)
+words2 = sentence.join("")
+print(words2)
 
 # # .format(): Allows inserting values into strings using {}
 # name = "Marvin"

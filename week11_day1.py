2fabf07e69d40dd61a578bba49609917da069875
Flipout23 <whenlimit11@gmail.com>
2025-10-30 13:33:04 +0000

    online

---
+++ week11_day1.py
@@ -0,0 +1,36 @@
+# # You can also use f-strings (Python 3.6+)
+# intro_fstring = f"My name is {name} and I am {age} years old."
+# print(intro_fstring)
+print(" Welcome to the Python Review Activity!")
+print("You'll review variables, strings, numbers, and print formatting.\n")
+
+# Step 1: Create Variables
+# TODO: Replace the values below with your own info
+first_name = "Felipe"
+age = 15
+favorite_color = "Green"
+favorite_number = 23
+
+#  Step 2: Practice String Operations
+# 1. Print your name in uppercase
+print(first_name.upper())
+
+# 2. Print how many letters are in your name
+print(len(first_name))
+
+# 3. Combine your name and favorite color into one message
+
+
+#  Step 3: Math Practice
+# Use arithmetic operators with your favorite number
+
+
+#  Step 4: User Input Practice
+# Ask the user two questions and combine answers
+
+
+
+
+# ⚙️ Step 5: Final Challenge (combine it all)
+# Use math and strings together
+
